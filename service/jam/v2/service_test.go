220e67ecdf07e66c535f9b42fe3d71f83f3aadb1
pmoieni <pmoieni06@gmail.com>
2022-12-29 20:57:24 +0330

    test public API for jam service

--- service/jam/v2/service_test.go
+++ service/jam/v2/service_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 	"github.com/hyphengolang/prelude/testing/is"
 )
 
@@ -25,7 +27,7 @@ func TestService(t *testing.T) {
 	h := NewService(ctx, mux)
 	srv := httptest.NewServer(h)
 
-	// var firstJam string
+	var firstJam string
 	t.Run("Create a new Jam room", func(t *testing.T) {
 		payload := `{
 			"name":     "John Doe",
@@ -36,28 +38,28 @@ func TestService(t *testing.T) {
 		res, _ := srv.Client().Post(srv.URL+"/api/v1/jam", "application/json", strings.NewReader(payload))
 		is.Equal(res.StatusCode, http.StatusCreated) // created a new resource
 
-		// loc, err := res.Location()
-		// 	// is.NoErr(err) // retrieve location
+		loc, err := res.Location()
+		is.NoErr(err) // retrieve location
 
-		// 	// firstJam = resource(loc.Path)
+		firstJam = resource(loc.Path)
 	})
 
-	// t.Run(`Connect to Jam room with id: `+firstJam, func(t *testing.T) {
-	// 	c1, _, _, err := ws.DefaultDialer.Dial(ctx, stripPrefix(srv.URL+"/ws/jam/"+firstJam))
-	// 	is.NoErr(err) // found first jam Session
+	t.Run(`Connect to Jam room with id: `+firstJam, func(t *testing.T) {
+		c1, _, _, err := ws.DefaultDialer.Dial(ctx, stripPrefix(srv.URL+"/ws/jam/"+firstJam))
+		is.NoErr(err) // found first jam Session
 
-	// 	t.Cleanup(func() { c1.Close() })
+		t.Cleanup(func() { c1.Close() })
 
-	// 	data := []byte("Hello World!")
-	// 	typ := []byte{1}
-	// 	m := append(typ, data...)
+		data := []byte("Hello World!")
+		typ := []byte{1}
+		m := append(typ, data...)
 
-	// 	err = wsutil.WriteClientBinary(c1, m)
-	// 	is.NoErr(err) // write to pool
+		err = wsutil.WriteClientBinary(c1, m)
+		is.NoErr(err) // write to pool
 
-	// 	res, err := wsutil.ReadServerBinary(c1)
-	// 	is.NoErr(err) // read from pool
+		res, err := wsutil.ReadServerBinary(c1)
+		is.NoErr(err) // read from pool
 
-	// 	is.Equal(res, m)
-	// })
+		is.Equal(res, m)
+	})
 }
