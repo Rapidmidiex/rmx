7cd97d320e350542430246fc11c2bb537377d13b
aboublef <kristopherab@gmail.com>
2022-12-30 22:21:59 +0000

    user repo working again

--- domain/user/postgres/repo.go
+++ domain/user/postgres/repo.go
@@ -1,123 +1,113 @@
-package user
+package postgres
 
-// // Definition of our User in the DB layer
-// type User struct {
-// 	// Primary key.
-// 	ID suid.UUID
-// 	// Unique. Stored as text.
-// 	Username string
-// 	// Unique. Stored as case-sensitive text.
-// 	Email email.Email
-// 	// Required. Stored as case-sensitive text.
-// 	Password password.PasswordHash
-// 	// Required. Defaults to current time.
-// 	CreatedAt time.Time
-// 	// TODO nullable, currently inactive
-// 	// UpdatedAt *time.Time
-// 	// TODO nullable, currently inactive
-// 	// DeletedAt *time.Time
-// }
+import (
+	"context"
+	"fmt"
+	"time"
 
-// type Repo interface {
-// 	Closer
-// 	Writer
-// 	Reader
-// }
+	"github.com/rog-golang-buddies/rmx/domain/user"
 
-// type ReadWriter interface {
-// 	Reader
-// 	Writer
-// }
+	"github.com/hyphengolang/prelude/types/email"
+	"github.com/hyphengolang/prelude/types/password"
+	"github.com/hyphengolang/prelude/types/suid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rog-golang-buddies/rmx/common/sql"
+)
 
-// type Writer interface {
-// 	internal.RepoWriter[internal.User]
-// }
+var (
+	ErrInvalidKey = fmt.Errorf("invalid key type")
+)
 
-// type Reader interface {
-// 	internal.RepoReader[internal.User]
-// }
+type User struct {
+	// Primary key.
+	ID suid.UUID
+	// Unique. Stored as text.
+	Username string
+	// Unique. Stored as case-sensitive text.
+	Email email.Email
+	// Required. Stored as case-sensitive text.
+	Password password.PasswordHash
+	// Required. Defaults to current time.
+	CreatedAt time.Time
+	// TODO nullable, currently inactive
+	// UpdatedAt *time.Time
+	// TODO nullable, currently inactive
+	// DeletedAt *time.Time
+}
 
-// type Closer interface {
-// 	internal.RepoCloser
-// }
+func (r *repo) ReadAll(ctx context.Context) ([]user.User, error) {
+	us, err := r.rh.Query(ctx, qrySelectMany, func(r pgx.Rows, u *user.User) error {
+		return r.Scan(&u.ID, &u.Email, &u.Username, &u.Password)
+	})
 
-// type repo struct {
-// 	ctx context.Context
-// 	c   *pgxpool.Pool
-// }
+	return us, err
+}
 
-// // This is not really required
-// func NewRepo(ctx context.Context, conn *pgxpool.Pool) Repo {
-// 	return &repo{ctx, conn}
-// }
+func (r *repo) Read(ctx context.Context, key any) (*user.User, error) {
+	var qry string
+	switch key.(type) {
+	case suid.UUID:
+		qry = qrySelectByID
+	case email.Email:
+		qry = qrySelectByEmail
+	case string:
+		qry = qrySelectByUsername
+	default:
+		return nil, ErrInvalidKey
+	}
 
-// func (r *repo) Context() context.Context {
-// 	if r.ctx != nil {
-// 		return r.ctx
-// 	}
-// 	return context.Background()
-// }
+	var u user.User
+	return &u, r.rh.QueryRow(ctx, qry, func(r pgx.Row) error { return r.Scan(&u.ID, &u.Username, &u.Email, &u.Password) }, key)
+}
 
-// func (r *repo) Close() { r.c.Close() }
+func (r *repo) Write(ctx context.Context, u *user.User) error {
+	args := pgx.NamedArgs{
+		"id":       u.ID,
+		"email":    u.Email,
+		"username": u.Username,
+		"password": u.Password,
+	}
 
-// func (r *repo) Insert(ctx context.Context, u *internal.User) error {
-// 	args := pgx.NamedArgs{
-// 		"id":       u.ID,
-// 		"email":    u.Email,
-// 		"username": u.Username,
-// 		"password": u.Password,
-// 	}
+	return r.rh.Exec(ctx, qryInsert, args)
+}
 
-// 	return psql.Exec(r.c, qryInsert, args)
-// }
+func (r *repo) Remove(ctx context.Context, key any) error {
+	var qry string
+	switch key.(type) {
+	case suid.UUID:
+		qry = qryDeleteByID
+	case email.Email:
+		qry = qryDeleteByEmail
+	case string:
+		qry = qryDeleteByUsername
+	default:
+		return ErrInvalidKey
+	}
+	return r.rh.Exec(ctx, qry, key)
+}
 
-// func (r *repo) SelectMany(ctx context.Context) ([]internal.User, error) {
-// 	return psql.Query(r.c, qrySelectMany, func(r pgx.Rows, u *internal.User) error {
-// 		return r.Scan(&u.ID, &u.Email, &u.Username, &u.Password)
-// 	})
-// }
+type repo struct {
+	rh sql.PSQLHandler[user.User]
+}
 
-// func (r *repo) Select(ctx context.Context, key any) (*internal.User, error) {
-// 	var qry string
-// 	switch key.(type) {
-// 	case suid.UUID:
-// 		qry = qrySelectByID
-// 	case email.Email:
-// 		qry = qrySelectByEmail
-// 	case string:
-// 		qry = qrySelectByUsername
-// 	default:
-// 		return nil, internal.ErrInvalidType
-// 	}
-// 	var u internal.User
-// 	return &u, psql.QueryRow(r.c, qry, func(r pgx.Row) error { return r.Scan(&u.ID, &u.Username, &u.Email, &u.Password) }, key)
-// }
+func New(conn *pgxpool.Pool) sql.RWRepo[user.User] {
+	r := &repo{
+		rh: sql.NewPSQLHandler[user.User](conn),
+	}
+	return r
+}
 
-// func (r *repo) Delete(ctx context.Context, key any) error {
-// 	var qry string
-// 	switch key.(type) {
-// 	case suid.UUID:
-// 		qry = qryDeleteByID
-// 	case email.Email:
-// 		qry = qryDeleteByEmail
-// 	case string:
-// 		qry = qryDeleteByUsername
-// 	default:
-// 		return internal.ErrInvalidType
-// 	}
-// 	return psql.Exec(r.c, qry, key)
-// }
+const (
+	qryInsert = `insert into "user" (id, email, username, password) values (@id, @email, @username, @password)`
 
-// const (
-// 	qryInsert = `insert into "user" (id, email, username, password) values (@id, @email, @username, @password)`
+	qrySelectMany = `select id, email, username, password from "user" order by id`
 
-// 	qrySelectMany = `select id, email, username, password from "user" order by id`
+	qrySelectByID       = `select id, email, username, password from "user" where id = $1`
+	qrySelectByEmail    = `select id, email, username, password from "user" where email = $1`
+	qrySelectByUsername = `select id, email, username, password from "user" where username = $1`
 
-// 	qrySelectByID       = `select id, email, username, password from "user" where id = $1`
-// 	qrySelectByEmail    = `select id, email, username, password from "user" where email = $1`
-// 	qrySelectByUsername = `select id, email, username, password from "user" where username = $1`
-
-// 	qryDeleteByID       = `delete from "user" where id = $1`
-// 	qryDeleteByEmail    = `delete from "user" where email = $1`
-// 	qryDeleteByUsername = `delete from "user" where username = $1`
-// )
+	qryDeleteByID       = `delete from "user" where id = $1`
+	qryDeleteByEmail    = `delete from "user" where email = $1`
+	qryDeleteByUsername = `delete from "user" where username = $1`
+)
--- domain/user/postgres/repo_test.go
+++ domain/user/postgres/repo_test.go
@@ -1,5 +1,5 @@
 // TODO - use the standard sql package instead of pgx
-package user
+package postgres_test
 
 import (
 	"context"
@@ -10,9 +10,14 @@ import (
 
 	"github.com/rog-golang-buddies/rmx/docker/container"
 	"github.com/rog-golang-buddies/rmx/docker/options"
+	"github.com/rog-golang-buddies/rmx/domain/user"
+	"github.com/rog-golang-buddies/rmx/domain/user/postgres"
 
 	"github.com/docker/go-connections/nat"
 	"github.com/hyphengolang/prelude/testing/is"
+	"github.com/hyphengolang/prelude/types/email"
+	"github.com/hyphengolang/prelude/types/password"
+	"github.com/hyphengolang/prelude/types/suid"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
@@ -53,8 +58,75 @@ func TestPostgresContainer(t *testing.T) {
 	is.NoErr(err) // create new instance of the connection pool
 	defer conn.Close()
 
-	_, err = conn.Exec(ctx, migration)
-	is.NoErr(err) // create new table
+	t.Run("migration", func(t *testing.T) {
+		_, err = conn.Exec(ctx, migration)
+		is.NoErr(err) // create new table
+	})
+
+	db := postgres.New(conn)
+
+	t.Run("write two users to table", func(t *testing.T) {
+		fizz := user.User{
+			ID:       suid.NewUUID(),
+			Email:    email.MustParse("fizz@mail.com"),
+			Username: "fizz",
+			Password: password.MustParse("fizz_pw_1").MustHash(),
+		}
+
+		err := db.Write(ctx, &fizz)
+		is.NoErr(err) // insert new user "fizz"
+
+		buzz := user.User{
+			ID:       suid.NewUUID(),
+			Email:    email.MustParse("buzz@mail.com"),
+			Username: "buzz",
+			Password: password.MustParse("buzz_pw_1").MustHash(),
+		}
+
+		err = db.Write(ctx, &buzz)
+		is.NoErr(err) // insert new user "buzz"
+
+		// us, err := db.SelectMany(ctx)
+		// is.NoErr(err)        // select all users
+		// is.Equal(len(us), 2) // should be a length of 2
+	})
+
+	t.Run("read two users from table", func(t *testing.T) {
+		us, err := db.ReadAll(ctx)
+		is.NoErr(err)        // select all users
+		is.Equal(len(us), 2) // should be a length of 2
+	})
+
+	t.Run("reject user with duplicate email/username", func(t *testing.T) {
+		fizz := user.User{
+			ID:       suid.NewUUID(),
+			Email:    email.MustParse("fuzz@mail.com"),
+			Username: "fizz",
+			Password: password.MustParse("fuzz_pw_1").MustHash(),
+		}
+
+		err := db.Write(ctx, &fizz)
+		is.True(err != nil) // duplicate user with username "fizz"
+	})
+
+	t.Run("select a user from the database using email/username", func(t *testing.T) {
+		u, err := db.Read(ctx, "fizz")
+		is.NoErr(err)                             // select user where username = "fizz"
+		is.NoErr(u.Password.Compare("fizz_pw_1")) // valid login
+
+		_, err = db.Read(ctx, email.MustParse("buzz@mail.com"))
+		is.NoErr(err) // select user where email = "buzz@mail.com"
+
+	})
+
+	t.Run("delete by username from database, return 1 user in database", func(t *testing.T) {
+		err := db.Remove(ctx, "fizz")
+		is.NoErr(err) // delete user where username == "fizz"
+
+		us, err := db.ReadAll(ctx)
+		is.NoErr(err)        // select all users
+		is.Equal(len(us), 1) // should be a length of 1
+	})
 }
 
 func TestPSQL(t *testing.T) {
