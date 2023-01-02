9d02ee9eebb58926f8a30af1faf93281e82f0e10
Harvey Sanders <sanders.harvey@gmail.com>
2023-01-02 02:11:52 -0500

    User migration

--- store/migration/000001_in-t_schema.up.sql
+++ store/migration/000001_in-t_schema.up.sql
@@ -1,9 +1,20 @@
-CREATE TABLE "jams"
-(
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE EXTENSION IF NOT EXISTS "citext";
+
+CREATE TABLE "jam" (
     "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
     "owner_id" uuid NOT NULL,
     "bpm" uint NOT NULL DEFAULT 120,
     "capacity" uint NOT NULL DEFAULT 5,
     "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE temp TABLE IF NOT EXISTS "user" (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    username text UNIQUE NOT NULL CHECK (username <> ''),
+    email citext UNIQUE NOT NULL CHECK (email ~ '^[a-zA-Z0-9.!#$%&’*+/=?^_\x60{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$'),
+    PASSWORD citext NOT NULL CHECK (PASSWORD <> ''),
+    created_at timestampz NOT NULL DEFAULT now()
 );
 
