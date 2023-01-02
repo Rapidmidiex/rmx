4a05ccf6bebf5d085a4a27f9fd0013f2a58348dd
Harvey Sanders <sanders.harvey@gmail.com>
2023-01-02 10:16:23 -0500

    Remove unsued code

--- internal/websocket/message.go
+++ internal/websocket/message.go
@@ -1,9 +1,5 @@
 package websocket
 
-import (
-	"encoding/json"
-)
-
 type WSMsgTyp int
 
 const (
@@ -28,19 +24,4 @@ func (m *message) parse(b []byte) {
 
 func (m *message) marshall() []byte {
 	return append([]byte{byte(m.typ)}, m.data...)
-}
-
-// Converts the given bytes to string
-func (m *message) readText() (string, error) {
-	return string(m.data), nil
-}
-
-// Converts the given bytes to JSON
-func (m *message) readJSON() (any, error) {
-	var v any
-	if err := json.Unmarshal(m.data, v); err != nil {
-		return nil, err
-	}
-
-	return v, nil
 }
