ffff6a7a527721a09798c1525f2989dfe00691b6
Harvey Sanders <sanders.harvey@gmail.com>
2023-01-06 15:18:53 -0500

    add missing import

--- internal/cmd/run.go
+++ internal/cmd/run.go
@@ -14,6 +14,7 @@ import (
 	"syscall"
 	"time"
 
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	"github.com/manifoldco/promptui"
 	"github.com/rapidmidiex/rmx/config"
 	db "github.com/rapidmidiex/rmx/internal/db"
