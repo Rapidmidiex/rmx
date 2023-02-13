ed6a5f37f2ad8956564dab50f69af0fa074836ad
aboublef <kristopherab@gmail.com>
2023-02-13 18:26:43 +0000

    unit test for jam service passes jam creation and search

--- internal/jam/http/service_test.go
+++ internal/jam/http/service_test.go
@@ -2,34 +2,105 @@ package service_test
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
+	"sync"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/rapidmidiex/rmx/internal/jam"
-	service "github.com/rapidmidiex/rmx/internal/jam/http"
+	service "github.com/rapidmidiex/rmx/internal/jam/http/v2"
+	"github.com/stretchr/testify/require"
 )
+
+var applicationJSON = "application/json"
 
 func TestService(t *testing.T) {
 	ctx := context.Background()
 
-	service.NewService(ctx, newTestStore())
+	h := service.New(ctx, newTestStore())
 
-	srv := httptest.NewServer(http.NotFoundHandler())
+	srv := httptest.NewServer(h)
 
 	t.Cleanup(func() { srv.Close() })
+
+	var roomID uuid.UUID
+
+	t.Run("POST /api/v1/jam", func(t *testing.T) {
+		payload := `
+		{
+			"name": "room-1",
+			"capacity": 2,
+			"bpm": 120
+		}`
+		log.Println(srv.URL + "/")
+		resp, err := srv.Client().Post(srv.URL+"/api/v1/jam", applicationJSON, strings.NewReader(payload))
+		require.NoError(t, err, "should not error")
+		require.Equal(t, http.StatusCreated, resp.StatusCode, "should return 201")
+
+		// get uuid from body
+		defer resp.Body.Close()
+
+		var jam jam.Jam
+		err = json.NewDecoder(resp.Body).Decode(&jam)
+		require.NoError(t, err, "should not error")
+
+		require.NotEmpty(t, jam.ID, "should have an ID")
+
+		roomID = jam.ID
+	})
+
+	t.Run("GET /api/v1/jam/{uuid}", func(t *testing.T) {
+
+		log.Println(srv.URL + "/api/v1/jam/" + roomID.String())
+
+		resp, err := srv.Client().Get(srv.URL + "/api/v1/jam/" + roomID.String())
+		require.NoError(t, err, "should not error")
+		require.Equal(t, http.StatusOK, resp.StatusCode, "should return 200")
+
+		defer resp.Body.Close()
+
+		var jam jam.Jam
+		err = json.NewDecoder(resp.Body).Decode(&jam)
+		require.NoError(t, err, "should not error")
+
+		require.Equal(t, roomID, jam.ID, "should have the same ID")
+	})
+
+	t.Run("GET /ws/v1/jam/{uuid}", func(t *testing.T) {
+		// create a new websocket connection
+	})
 }
 
-type testStore struct{}
+type testStore struct {
+	mu sync.Mutex
+	m  map[uuid.UUID]jam.Jam
+}
 
 func newTestStore() *testStore {
-	s := &testStore{}
+	s := &testStore{
+		m: make(map[uuid.UUID]jam.Jam),
+	}
 	return s
 }
 
-func (s *testStore) CreateJam(context.Context, jam.Jam) (jam.Jam, error) {
-	panic("implement me")
+func (s *testStore) CreateJam(ctx context.Context, j jam.Jam) (jam.Jam, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	created := jam.Jam{
+		ID:       uuid.New(),
+		Name:     j.Name,
+		Capacity: j.Capacity,
+		BPM:      j.BPM,
+	}
+
+	s.m[created.ID] = created
+	return created, nil
 }
 
 func (s *testStore) GetJams(context.Context) ([]jam.Jam, error) {
@@ -37,5 +108,12 @@ func (s *testStore) GetJams(context.Context) ([]jam.Jam, error) {
 }
 
 func (s *testStore) GetJamByID(ctx context.Context, id uuid.UUID) (jam.Jam, error) {
-	panic("implement me")
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	j, ok := s.m[id]
+	if !ok {
+		return jam.Jam{}, errors.New("jam not found")
+	}
+
+	return j, nil
 }
--- internal/jam/http/v2/service.go
+++ internal/jam/http/v2/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	service "github.com/rapidmidiex/rmx/internal/http"
 	"github.com/rapidmidiex/rmx/internal/jam"
@@ -19,11 +20,14 @@ type store interface {
 type Service struct {
 	m service.Service
 
-	s store
+	r store
 }
 
-func New() *Service {
-	s := &Service{}
+func New(ctx context.Context, store store) *Service {
+	s := &Service{
+		m: service.New(),
+		r: store,
+	}
 	s.routes()
 	return s
 }
@@ -33,15 +37,77 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Service) routes() {
-	s.m.Get("/api/v1/jam", http.NotFound)
-	s.m.Post("/api/v1/jam", http.NotFound)
-	s.m.Get("/api/v1/jam/{uuid}", http.NotFound)
+	s.m.Post("/api/v1/jam", s.handleCreateJam())
+	// s.m.Get("/api/v1/jam", http.NotFound)
+	s.m.Get("/api/v1/jam/{uuid}", s.handleGetJam())
 
 	s.m.Get("/ws/jam/{uuid}", http.NotFound)
 }
 
 func (s *Service) handleCreateJam() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO
+	type Q struct {
+		Name     string `json:"name"`
+		Capacity uint   `json:"capacity"`
+		BPM      uint   `json:"bpm"`
 	}
+
+	newJam := func(w http.ResponseWriter, r *http.Request) (jam.Jam, error) {
+		var q Q
+		if err := s.m.Decode(w, r, &q); err != nil {
+			return jam.Jam{}, err
+		}
+
+		j := jam.Jam{
+			Name:     q.Name,
+			Capacity: q.Capacity,
+			BPM:      q.BPM,
+		}
+		return j, nil
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		j, err := newJam(w, r)
+		if err != nil {
+			s.m.Respond(w, r, err, http.StatusBadRequest)
+			return
+		}
+
+		created, err := s.r.CreateJam(r.Context(), j)
+		if err != nil {
+			s.m.Respond(w, r, err, http.StatusInternalServerError)
+			return
+		}
+
+		s.m.Respond(w, r, created, http.StatusCreated)
+	}
+}
+
+func (s *Service) handleGetJam() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// NOTE move to middleware
+		uid, err := parseUUID(r)
+		if err != nil {
+			s.m.Respond(w, r, uid, http.StatusBadRequest)
+			return
+		}
+
+		jam, err := s.r.GetJamByID(r.Context(), uid)
+		if err != nil {
+			s.m.Respond(w, r, err, http.StatusNotFound)
+			return
+		}
+
+		s.m.Respond(w, r, jam, http.StatusOK)
+	}
+}
+
+func (s *Service) handleP2PConn() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func parseUUID(r *http.Request) (uuid.UUID, error) {
+	p := chi.URLParam(r, "uuid")
+	return uuid.Parse(p)
 }
